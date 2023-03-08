04558f81525e68ed0042f338f2a394511461e055
Thái Thị Thanh Ngân <94752353+ThanhNgan110@users.noreply.github.com>
2023-03-08 23:44:36 +0700

    ngan

--- Validator.js
+++ Validator.js
@@ -7,13 +7,13 @@ const confirmPassword = document.getElementById('password_confirmation');
 
 
 
-form.addEventListener('submit', e =>{
-     e.preventDefault();
-     validateInputs();
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    validateInputs();
 
 });
 
-const setError = (element, message) =>{
+const setError = (element, message) => {
     const inputControl = element.parentElement;
     console.log(inputControl);
     const errorDisplay = inputControl.querySelector('.error');
@@ -21,11 +21,11 @@ const setError = (element, message) =>{
 
     inputControl.classList.add('error');
     inputControl.classList.remove('success');
-    
+
 };
 
 
-const setSuccess = element =>{
+const setSuccess = element => {
     const inputControl = element.parentElement;
     console.log(inputControl);
     const errorDisplay = inputControl.querySelector('.error');
@@ -37,64 +37,64 @@ const setSuccess = element =>{
 };
 
 
-const isEmailvalid = email=>{
-    const reg =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const isEmailvalid = email => {
+    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return reg.test(email);
 
 
 };
 
 
-const validateInputs = ()=>{ 
+const validateInputs = () => {
 
-   const fullnameValue = fullname.value.trim();
-   const emailValue = email.value.trim();
-   const passwordValue = password.value.trim();
-   const passwordConfirmed = confirmPassword.value.trim();
+    const fullnameValue = fullname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const passwordConfirmed = confirmPassword.value.trim();
 
-   if(fullnameValue === ''){
-    setError(fullname, 'Fullname không được để rỗng');
-   }
-   else{
-    setSuccess(fullname)
-    
-   }
+    if (fullnameValue === '') {
+        setError(fullname, 'Fullname không được để rỗng');
+    }
+    else {
+        setSuccess(fullname)
 
-   if(emailValue === ''){
-    setError(email,'Email không được rỗng');
+    }
 
-   }
-   else if(!isEmailvalid){
-    setError(email, 'Email không đúng định dạng');
-   }
-   else{
-    setSuccess(email)
+    if (emailValue === '') {
+        setError(email, 'Email không được rỗng');
 
-   }
+    }
+    else if (!isEmailvalid) {
+        setError(email, 'Email không đúng định dạng');
+    }
+    else {
+        setSuccess(email)
 
-   if(passwordValue === ''){
-    setError(password, 'Password không được để rỗng');  
-   }
-   else if(passwordValue.lenght<8){
-    setError(password, 'Password tối thiểu ít nhất 8 ký tự ');
+    }
 
-   }
-   else{
-    setSuccess(password);
-   }
+    if (passwordValue === '') {
+        setError(password, 'Password không được để rỗng');
+    }
+    else if (passwordValue.lenght < 8) {
+        setError(password, 'Password tối thiểu ít nhất 8 ký tự ');
 
-   if(passwordConfirmed ===''){
-    setError(confirmPassword, 'Please enter confirm your password  ');  
+    }
+    else {
+        setSuccess(password);
+    }
 
-   }
-   else if(passwordValue  !== password ){
-    setError(confirmPassword,'Password incorrect');
+    if (passwordConfirmed === '') {
+        setError(confirmPassword, 'Please enter confirm your password  ');
 
-   }
-   else{
-    setSuccess(confirmPassword);
+    }
+    else if (passwordValue !== password) {
+        setError(confirmPassword, 'Password incorrect');
 
-   }
+    }
+    else {
+        setSuccess(confirmPassword);
+
+    }
 };
 
 
